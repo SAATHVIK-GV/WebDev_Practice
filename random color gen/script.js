aff45d04f09551eb477203a8100e603b988e95ca
GANDAPODI V SAATHVIK <113020084+SAATHVIK-GV@users.noreply.github.com>
2024-03-20 18:56:37 +0530

    Add files via upload

---
+++ random color gen/script.js
@@ -0,0 +1,7 @@
+let arr = ["red","green","blue","beige","yellow","orange","violet","brown","black","aqua","coral"]
+console.log(arr[Math.round(Math.random()*10)]);
+document.getElementById("box1").style.backgroundColor = arr[Math.round(Math.random()*10)];
+document.getElementById("box2").style.backgroundColor = arr[Math.round(Math.random()*10)];
+document.getElementById("box3").style.backgroundColor = arr[Math.round(Math.random()*10)];
+document.getElementById("box4").style.backgroundColor = arr[Math.round(Math.random()*10)];
+document.getElementById("box5").style.backgroundColor = arr[Math.round(Math.random()*10)];
